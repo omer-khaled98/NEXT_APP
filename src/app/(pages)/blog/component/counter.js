c3686f03c48c678c5db4e4dce42b94856f7c7d15
Omer kh <omerkhaled123789456@gmail.com>
2024-08-31 07:03:43 +0300

    new

--- src/app/(pages)/blog/component/counter.js
+++ src/app/(pages)/blog/component/counter.js
@@ -3,18 +3,14 @@
 import React, { useState } from "react";
 
 const Post = ({ content }) => {
-  // State for the like counter
   const [likes, setLikes] = useState(0);
-
-  // Function to handle the like button click
 
   const handleLike = () => {
     setLikes(likes + 1);
   };
 
-  // Function to handle the dislike button click
   const handleDislike = () => {
-    setLikes(likes > 0 ? likes - 1 : 0); // Prevent negative values
+    setLikes(likes > 0 ? likes - 1 : 0);
   };
 
   return (
@@ -22,7 +18,7 @@ const Post = ({ content }) => {
       <div className="content">
         <p>{content}</p>
       </div>
-      {/* Render footer on client side */}
+
       <footer>
         <button type="button" class="btn btn-primary" onClick={handleLike}>
           {" "}
--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,27 +1,3 @@
-// import clientPromise from "@/lib/mongodb";
-// import React from "react";
-
-// export async function GET(request) {
-//   // request.nextUrl.searchParams  for fetch data by id
-
-//   const client = await clientPromise;
-
-//   const db = client.db("nextjs");
-//   const posts = await db.collection("posts").find({}).toArray();
-//   return new Response(JSON.stringify(posts), { status: 200 });
-// }
-// export async function POST(request) {
-//   // request.nextUrl.searchParams  for fetch data by id
-
-//   const client = await clientPromise;
-
-//   const db = client.db("nextjs");
-
-//   const data = await request.json();
-//   await db.collection("posts").insertOne(data);
-//   console.log("body", data);
-//   return new Response("success", { status: 200 });
-// }
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
