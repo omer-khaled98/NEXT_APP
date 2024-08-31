1dede2470d32d562b22664e88c247c415d90abe6
Omer kh <omerkhaled123789456@gmail.com>
2024-08-31 07:12:54 +0300

    depoly

--- src/lib/fetchPostByID.js
+++
@@ -1,6 +0,0 @@
-// import { connectToDatabase } from "./mongodb";
-
-// export async function fetchPostByID(id) {
-//   const { db } = await connectToDatabase();
-//   return db.collection("posts").findOne({ id: parseInt(id, 10) });
-// }
